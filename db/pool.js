7cf15f98fa637ac730ad734f04a6279e2fae554e
Tifa004 <mostafasakr01@hotmail.com>
2026-03-22 05:36:07 +0200

    further render adjustments

--- db/pool.js
+++ db/pool.js
@@ -12,4 +12,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Required for some Render connections
+  }
 });
