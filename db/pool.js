c3fbd49e6e049ebc0efb1cc10c96889d67c52cd6
Tifa004 <mostafasakr01@hotmail.com>
2026-03-22 05:30:39 +0200

    Changed the pool.js to support render db

--- db/pool.js
+++ db/pool.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const { Pool } = require("pg");
 
-// All of the following properties should be read from environment variables
-// We're hardcoding them here for simplicity
+
+// module.exports = new Pool({
+//   host: process.env.DB_HOST, // or wherever the db is hosted
+//   user: process.env.DB_USER,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT // The default port
+// });
+
 module.exports = new Pool({
-  host: process.env.DB_HOST, // or wherever the db is hosted
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT // The default port
+  connectionString: process.env.DATABASE_URL,
 });
